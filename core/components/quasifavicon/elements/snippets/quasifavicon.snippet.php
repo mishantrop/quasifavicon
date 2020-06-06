b1714c3a7aae18725926a2b53b146b90002cdfcc
Mikhail <mishantrop@users.noreply.github.com>
2020-06-06 23:39:05 +0300

    Simplify

--- core/components/quasifavicon/elements/snippets/quasifavicon.snippet.php
+++ core/components/quasifavicon/elements/snippets/quasifavicon.snippet.php
@@ -9,54 +9,57 @@
  ]]
 */
 if (version_compare(PHP_VERSION, '5.4.0') < 0) {
-    return '<!-- Your version of PHP is very ancient -->';
+  return '<!-- Your version of PHP is very ancient -->';
 }
 
-$output = '';
+$tags = [];
 // Цвет фона. Если не указан, то не применяется.
-$backgroundColor = (string)$modx->getOption('backgroundColor', $scriptProperties, '');
+$backgroundColor = $modx->getOption('backgroundColor', $scriptProperties, '');
 // Путь до изображения
-$image = (string)$modx->getOption('image', $scriptProperties, '');
+$image = $modx->getOption('image', $scriptProperties, '');
 // Нужно ли генерировать favicon.ico
 $ico = (int)$modx->getOption('ico', $scriptProperties, 0);
 // Сниппет, создающий миниатюры (phpthumbof, pthumb, phpthumbon)
-$snippet = (string)$modx->getOption('snippet', $scriptProperties, 'phpthumbof');
+$snippet = $modx->getOption('snippet', $scriptProperties, 'phpthumbof');
 // Цвет вкладки для мобильных браузеров
-$tabColor = (string)$modx->getOption('tabColor', $scriptProperties, '');
+$tabColor = $modx->getOption('tabColor', $scriptProperties, '');
 
 // Apple
-$sizes = ['57x57', '144x144', '72x72', '144x144', '60x60', '120x120', '76x76', '152x152'];
-foreach ($sizes as $size) {
+$sizesApple = [ '57x57', '144x144', '72x72', '144x144', '60x60', '120x120', '76x76', '152x152' ];
+foreach ($sizesApple as $size) {
 	$as = explode('x', $size);
 	$options = 'w='.$as[0].'&h='.$as[1].'&zc=1&f=png';
 	if (!empty($backgroundColor)) {
 	    $options .= '&bg='.$backgroundColor;
 	}
-	$output .= '<link rel="apple-touch-icon-precomposed" sizes="'.$size.'" href="'.$modx->runSnippet($snippet, ['input' => $image, 'options' => $options]).'" />'.PHP_EOL;
+  $thumbPath = $modx->runSnippet($snippet, ['input' => $image, 'options' => $options]);
+	$tags[] = '<link rel="apple-touch-icon-precomposed" sizes="'.$size.'" href="'.$thumbPath.'">';
 }
 
 // Classic
-$sizes = ['32x32', '16x16', '96x96', '128x128', '196x196'];
-foreach ($sizes as $size) {
+$sizesClassic = [ '32x32', '16x16', '96x96', '128x128', '196x196' ];
+foreach ($sizesClassic as $size) {
 	$as = explode('x', $size);
 	$options = 'w='.$as[0].'&h='.$as[1].'&zc=1&f=png';
 	if (!empty($backgroundColor)) {
 		$options .= '&bg='.$backgroundColor;
 	}
-	$output .= '<link rel="icon" type="image/png" sizes="'.$size.'" href="'.$modx->runSnippet($snippet, ['input' => $image, 'options' => $options]).'" />'.PHP_EOL;
+  $thumbPath = $modx->runSnippet($snippet, ['input' => $image, 'options' => $options]);
+	$tags[] = '<link rel="icon" type="image/png" sizes="'.$size.'" href="'.$thumbPath.'">';
 }
 
 // Microsoft
-$output .= '<meta name="msapplication-TileColor" content="#FFFFFF" />'.PHP_EOL;
-$output .= '<meta name="msapplication-TileImage" content="'.$modx->runSnippet($snippet, ['input' => $image, 'options'=>'w=144&h=144&zc=1&f=png&bg='.$backgroundColor]).'" />'.PHP_EOL;
-$sizes = ['70x70', '150x150', '310x310'];
-foreach ($sizes as $size) {
-	$as = explode('x',$size);
+$tags[] = '<meta name="msapplication-TileColor" content="#FFFFFF" />';
+$tags[] = '<meta name="msapplication-TileImage" content="'.$modx->runSnippet($snippet, ['input' => $image, 'options'=>'w=144&h=144&zc=1&f=png&bg='.$backgroundColor]).'">';
+$sizesMicrosoft = ['70x70', '150x150', '310x310'];
+foreach ($sizesMicrosoft as $size) {
+	$as = explode('x', $size);
 	$options = 'w='.$as[0].'&h='.$as[1].'&zc=1&f=png';
 	if (!empty($backgroundColor)) {
 		$options .= '&bg='.$backgroundColor;
 	}
-	$output.='<meta name="msapplication-square'.$size.'logo" content="'.$modx->runSnippet($snippet, ['input' => $image, 'options' => $options]).'" />'.PHP_EOL;
+  $thumbPath = $modx->runSnippet($snippet, ['input' => $image, 'options' => $options]);
+	$tags[] = '<meta name="msapplication-square'.$size.'logo" content="'.$thumbPath.'">';
 }
 
 // favicon.ico
@@ -65,14 +68,15 @@ if ($ico === 1) {
 	if (!empty($backgroundColor)) {
 		$options .= '&bg='.$backgroundColor;
 	}
-	$output .= '<link rel="shortcut icon" href="'.$modx->runSnippet($snippet, ['input' => $image, 'options' => $options]).'" type="image/x-icon" />'.PHP_EOL;
-	$output .= '<link rel="icon" href="'.$modx->runSnippet($snippet, ['input' => $image, 'options'=>$options]).'" type="image/x-icon" />'.PHP_EOL;
+  $thumbPath = $modx->runSnippet($snippet, ['input' => $image, 'options' => $options]);
+	$tags[] = '<link rel="shortcut icon" href="'.$thumbPath.'" type="image/x-icon">';
+	$tags[] = '<link rel="icon" href="'.$thumbPath.'" type="image/x-icon">';
 }
 
 if (!empty($tabColor)) {
-	$output .= '<meta name="theme-color" content="#'.$tabColor.'" />'.PHP_EOL;
-	$output .= '<meta name="msapplication-navbutton-color" content="#'.$tabColor.'" />'.PHP_EOL;
-	$output .= '<meta name="apple-mobile-web-app-status-bar-style" content="#'.$tabColor.'" />'.PHP_EOL;
+	$tags[] = '<meta name="theme-color" content="#'.$tabColor.'">';
+	$tags[] = '<meta name="msapplication-navbutton-color" content="#'.$tabColor.'">';
+	$tags[] = '<meta name="apple-mobile-web-app-status-bar-style" content="#'.$tabColor.'">';
 }
 
-return $output;
+return implode(PHP_EOL, $tags);
